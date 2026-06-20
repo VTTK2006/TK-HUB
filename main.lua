dcf616d1e0db422035cae6fcfa674a48e164716d
Tuấn Khải <117251417+VTTK2006@users.noreply.github.com>
2026-06-20 15:34:53 +0700

    Update script links in main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
-spawn(function() loadstring(game:HttpGet("LINK_RAW_DRAGSCRIPT"))() end)
-spawn(function() loadstring(game:HttpGet("LINK_RAW_LOCALSCRIPT"))() end)
-spawn(function() loadstring(game:HttpGet("LINK_RAW_HOMEDATASCRIPT"))() end)
+spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/VTTK2006/TK-HUB/refs/heads/main/DragScript.lua"))() end)
+spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/VTTK2006/TK-HUB/refs/heads/main/LocalScript.lua"))() end)
+spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/VTTK2006/TK-HUB/refs/heads/main/HomeDataScript.lua"))() end)
